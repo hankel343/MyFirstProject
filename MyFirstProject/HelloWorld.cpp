1d55204d5785466cf35961291de555d2e4ac7054
hankel343 <57358008+hankel343@users.noreply.github.com>
2020-08-25 17:15:07 -0500

    Add project files.

---
+++ MyFirstProject/HelloWorld.cpp
@@ -0,0 +1,21 @@
+/*
+Hankel Haldin
+C++ 2020
+Due: 8/31/2020
+Lab 1 Introduction to C++ Programming
+Uploading/downloading files from GitHub
+*/
+
+//Preprocessor directive to include the input/output library that includes cout/cin/std keywords.
+#include <iostream>
+
+//This is the main function of the program. This is where the program begins execution.
+int main()
+{
+	//The following two lines print text to the console using the cout keyword.
+	std::cout << "Hello, world!";
+	std::cout << "\nBlue";
+	
+	//The main function must have a return value, and this is shown below. This lets the compiler know the program executed successfully.
+	return 0;
+}
